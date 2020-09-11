ff2e595204a28d40410964ef6d28bd8b58cebb39
jungbada269 <57929751+jungbada269@users.noreply.github.com>
2020-09-11 11:32:11 +0900

    Create 짝수와 홀수.js

---
+++ 짝수와 홀수.js
@@ -0,0 +1,13 @@
+function solution(num) {
+    var answer = '';
+    
+    if (num % 2 == 0)
+        {
+            answer = "Even";
+        }
+    else
+        {
+            answer = "Odd";
+        }
+    return answer;
+}
