614f74295c3474bc0343d549dc0a964f83aa7eac
Bada Jung <57929751+badasea@users.noreply.github.com>
2022-06-01 22:38:33 +0900

    Create 소수 만들기.js

---
+++ programmers/javascript/소수 만들기.js
@@ -0,0 +1,26 @@
+function solution(nums) {
+    var answer = 0;
+    var i,j,k;
+
+  for (i = 0; i < nums.length - 2; i++) {
+    for (j = i + 1; j < nums.length - 1; j++) {
+      for (k = j + 1; k < nums.length; k++) {
+        if (isPrime(nums[i] + nums[j] + nums[k])) {
+          answer++;
+        }
+      }
+    }
+  }
+    
+  return answer;
+}
+
+const isPrime = (n) => {
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
